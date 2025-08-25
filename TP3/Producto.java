f85923b6e0e84a70f216997945e30011e6649038
RebecaSteirensis <angelarebecagomezsteirensis@gmail.com>
2025-08-25 19:55:37 -0300

    jiji

--- TP3/Producto.java
+++ TP3/Producto.java
@@ -33,50 +33,99 @@ public class Producto
     }
 
     // Setters privados
-    private void setCodigo(int p_codigo){ this.codigo = p_codigo; }
-    private void setRubro(String p_rubro){ this.rubro = p_rubro; }
-    private void setDescripcion(String p_desc){ this.descripcion = p_desc; }
-    private void setCosto(double p_costo){ this.costo = p_costo; }
-    private void setStock(int p_stock){ this.stock = p_stock; }
-    private void setPorcPtoRepo(double p_porc){ this.porcPtoRepo = p_porc; }
-    private void setExistMinima(int p_existMinima){ this.existMinima = p_existMinima; }
-    private void setLaboratorio(Laboratorio p_lab){ this.laboratorio = p_lab; }
+    private void setCodigo(int p_codigo){ 
+        this.codigo = p_codigo; 
+    }
+    
+    private void setRubro(String p_rubro){
+        this.rubro = p_rubro; 
+    }
+    
+    private void setDescripcion(String p_desc){
+        this.descripcion = p_desc; 
+    }
+    
+    private void setCosto(double p_costo){
+        this.costo = p_costo; 
+    }
+    
+    private void setStock(int p_stock){
+        this.stock = p_stock; 
+    }
+    private void setPorcPtoRepo(double p_porc){
+        this.porcPtoRepo = p_porc; 
+    }
+    
+    private void setExistMinima(int p_existMinima){
+        this.existMinima = p_existMinima; 
+    }
+    
+    private void setLaboratorio(Laboratorio p_lab){
+        this.laboratorio = p_lab; 
+    }
 
     // Getters públicos
-    public int getCodigo(){ return this.codigo; }
-    public String getRubro(){ return this.rubro; }
-    public String getDescripcion(){ return this.descripcion; }
-    public double getCosto(){ return this.costo; }
-    public int getStock(){ return this.stock; }
-    public double getPorcPtoRepo(){ return this.porcPtoRepo; }
-    public int getExistMinima(){ return this.existMinima; }
-    public Laboratorio getLaboratorio(){ return this.laboratorio; }
+    public int getCodigo(){
+        return this.codigo; 
+    }
+    
+    public String getRubro(){
+        return this.rubro; 
+    }
+    
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+    
+    public double getCosto(){
+        return this.costo; 
+    }
+    
+    public int getStock(){
+        return this.stock; 
+    }
+    
+    public double getPorcPtoRepo(){
+        return this.porcPtoRepo; 
+    }
+    
+    public int getExistMinima(){
+        return this.existMinima; 
+    }
+    
+    public Laboratorio getLaboratorio(){
+        return this.laboratorio; }
 
     // Métodos públicos
-    public void ajuste(int p_cantidad){ this.setStock(this.getStock() + p_cantidad); }
+    public void ajuste(int p_cantidad){
+        this.setStock(this.getStock() + p_cantidad); 
+    }
 
-    public double stockValorizado(){ return this.getStock() * this.getCosto() * 1.12; }
+    public double stockValorizado(){
+        return this.getStock() * this.getCosto() * 1.12; 
+    }
 
-    public double precioLista(){ return this.getCosto() * 1.12; }
+    public double precioLista(){
+        return this.getCosto() * 1.12; 
+    }
 
-    public double precioContado(){ return this.precioLista() * 0.95; }
+    public double precioContado(){
+        return this.precioLista() * 0.95; 
+    }
 
-    public void ajustarPtoRepo(double p_porc){ this.setPorcPtoRepo(p_porc); }
+    public void ajustarPtoRepo(double p_porc){
+        this.setPorcPtoRepo(p_porc); 
+    }
 
-    public void ajustarExistMin(int p_cant){ this.setExistMinima(p_cant); }
+    public void ajustarExistMin(int p_cant){
+        this.setExistMinima(p_cant); 
+    }
 
     public String mostrar(){
-        return this.getLaboratorio().mostrar() +
-               "\nRubro: " + this.getRubro() +
-               "\nDescripción: " + this.getDescripcion() +
-               "\nPrecio Costo: " + this.getCosto() +
-               "\nStock: " + this.getStock() +
-               " - Stock Valorizado: $" + this.stockValorizado();
+        return this.getLaboratorio().mostrar() + "\nRubro: " + this.getRubro() + "\nDescripción: " + this.getDescripcion() + "\nPrecio Costo: " + this.getCosto() + "\nStock: " + this.getStock() + " - Stock Valorizado: $" + this.stockValorizado();
     }
 
     public String mostrarLinea(){
-        return this.getDescripcion() +
-               " - Lista: $" + this.precioLista() +
-               " - Contado: $" + this.precioContado();
+        return this.getDescripcion() + " - Lista: $" + this.precioLista() + " - Contado: $" + this.precioContado();
     }
 }
